ac1030fb6324fbd3122b6dc3c9f153f6df2e1899
DamianGRO <fiszher@gmail.com>
2020-06-21 21:50:18 +0200

    funkcja weryfikujaca obecnosc elementu

--- src/test/java/Testowanie1.java
+++ src/test/java/Testowanie1.java
@@ -2,10 +2,27 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Testowanie1 {
+
+
+    public static boolean ifElementPresent (WebDriver driver, By by)
+    {
+        try
+        {
+            driver.findElement(by);
+            return true;
+        }
+        catch (NoSuchElementException e)
+        {
+            System.out.println("Nie znaleziono elementu");
+            return false;
+        }
+    }
 
 
     private WebDriver driver;
@@ -26,10 +43,6 @@ public class Testowanie1 {
     @Test
     public void testujemyNegatywnie() {
 
-        /* System.setProperty("webdriver.chrome.driver", "E:\\mvnrepository.com\\chromeDriver_Wersja_83\\chromedriver.exe");
-        driver = new ChromeDriver();
-        String url = "https://www.google.pl/";
-        driver.navigate().to(url); */
         String wynikTestu = driver.getTitle();
         Assert.assertEquals(wynikTestu, "Kon");
 
@@ -40,8 +53,26 @@ public class Testowanie1 {
 
         String wynikTestu = driver.getTitle();
         Assert.assertEquals(wynikTestu, "Google");
+    }
 
+    @Test
+    public void pozytywnaObecnoscElementu() {
 
+        String wynikTestu = driver.getTitle();
+        Assert.assertEquals(wynikTestu, "Google");
+        driver.findElement(By.xpath("//*[@id=\"gbwa\"]/div/a"));
+
+        ifElementPresent(driver, By.xpath("//*[@id=\"gbwa\"]/div/a"));
+        Assert.assertTrue(ifElementPresent(driver, By.xpath("//*[@id=\"gbwa\"]/div/a")));
+    }
+
+    @Test
+    public void negatywnaObecnoscElementu() {
+
+        String wynikTestu = driver.getTitle();
+        Assert.assertEquals(wynikTestu, "Google");
+        ifElementPresent(driver, By.xpath("ccccccccc"));
+        Assert.assertTrue(ifElementPresent(driver, By.xpath("ccccccccc")));
     }
 
     @After
